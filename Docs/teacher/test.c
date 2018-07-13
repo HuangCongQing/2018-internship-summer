b2f205ae86695da6c1ac360a95dff1ea1c7edfca
HuangChongqing <1756260160@qq.com>
2018-07-13 10:23:05 +0800

    teacher

---
+++ Docs/teacher/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+void main(void)
+{
+	int cnt = 5;     
+	double height = 176.32;
+	char color[32] = "blue";
+	char tt = 'A';
+
+	
+	printf("cnt=%d\n",cnt);
+	printf("heigt = %lf\n",height);
+	printf("color=%s\n",color);
+	printf("tt=%c\n",tt);
+
+}
